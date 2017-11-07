070002eaa083dfe6580144b7602b6f0d93520750
user <user@LAPTOP-OM93RJFG>
2017-11-07 22:43:44 +0900

    3rd

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ import chat.client.ChatClient;
 import chat.server.ChatServer;
 
 public class Test {
-
+// dummy chatter test class
 	public static void main(String[] args) {
 		ChatClient cc = new ChatClient();
 		ChatServer cs = new ChatServer();
